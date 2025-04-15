f1722a61584007201785c476e6aabe1e45bc5f40
BegliardoFrancisco <panchobegliardo@gmail.com>
2025-04-15 16:18:38 -0300

    icono ubicacion y correciones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import '../styles/icon.css';
 import GitHubIcon from '../assets/GithubIcon.jsx';
 import MailIcon from '../assets/MailIcon.jsx';
 import LinkintIcon from '../assets/LinkintIcon.jsx';
-
+import UbicationIcon from '../assets/UbicationIcon.jsx';
 const Home = () => {
 
     return (
@@ -13,7 +13,10 @@ const Home = () => {
                     <h1>CONTACT</h1>
                     <h2>Francisco Hugo Begliardo</h2>
                     <h3>24 años</h3>
-
+                    <div className='CardContact'>
+                        <UbicationIcon />
+                        <a href='https://maps.app.goo.gl/QyQNcJ7376VPLypU6'>Cordoba, Cordoba, Argentina</a>
+                    </div>
                     <div className='CardContact'>
                         <GitHubIcon />
                         <a href='https://github.com/BegliardoFrancisco'>https://github.com/BegliardoFrancisco</a>
@@ -23,7 +26,7 @@ const Home = () => {
                         <a href='mailto:franciscobegliardo@gmail.com'>franciscobegliardo@gmail.com</a>
                     </div>
                     <div className='CardContact'>
-                        <LinkintIcon />
+                        <LinkintIcon/>
                         <a href='https://www.linkedin.com/in/francisco-begliardo-2a07a2297/'>https://www.linkedin.com/in/francisco-begliardo-2a07a2297/</a>
                     </div>
                 </section>
