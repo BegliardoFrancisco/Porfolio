986fe7fbc443fce9305e57e242a43420e74e2475
BegliardoFrancisco <panchobegliardo@gmail.com>
2025-04-22 11:40:09 -0300

    feat: add SectionStudy component with educational background details

---
+++ src/components/SectionStudy.jsx
@@ -0,0 +1,68 @@
+
+
+
+
+const SectionStudy = () => {
+    const studies = [
+        {
+
+            img: "https://example.com/image0.jpg",
+            title: "Analista en sistemas de información",
+            institution: "Universidad Tecnológica Nacional - Facultad Regional Cordoba UTN-FRC",
+            year: "2019 - 2025",
+            description: "Algoritmos y estructuras de datos, programacion orientada a objetos, programacion web, bases de datos, redes de computadoras, sistemas operativos, Analisis & Diseño de sistemas, arquitectura de computadoras"		
+        },
+
+
+        {
+            img : "https://example.com/image1.jpg",
+            title: "Curador de datos",
+            institution: "Capacitate para el empleo - Fundación Carlos Slim",
+            year: "2020",
+            description: "Aprendi a manipular, limpiar y optimizar la informacion que se encuetnra en una base de datos"
+        },
+        {
+            img : "https://example.com/image2.jpg", 
+            title: "Administrador de Base de datos",
+            institution: "Capacitate para el empleo - Fundación Carlos Slim",
+            year: "2020",
+            description:"Crear,consultar y adminitrar de base datos relaciones SQL "
+        },
+        {
+            img: "https://example.com/image3.jpg",
+            title: "Desarrollador Backend",
+            institution: "Capacitate para el empleo - Fundación Carlos Slim",
+            year: "2020",
+            description: "Desarrollar y mantener la lógica del servidor y la base de datos, modelo Cliente Servidor"
+        },
+        {
+            img: "https://example.com/image4.jpg",
+            title: "Desarrollador Frontend",
+            institution: "Capacitate para el empleo - Fundación Carlos Slim",
+            year: "2020",
+            description: "Desarrollar y mantener la parte visual de una aplicacion web"
+        },
+        {
+            img: "https://example.com/image5.jpg",
+            title: "Administrador de Servidores",
+            institution: "Capacitate para el empleo - Fundación Carlos Slim",
+            year: "2020",
+            description: "Sistemas operativos, bashScripting, Instalacion y configuracion de servidores (web, BDD, impresion,correo, Raids)"
+        },
+        {
+            img: "https://example.com/image6.jpg",
+            title: "Programador (Orientado a Objetos)",	
+            institution: "Capacitate para el empleo - Fundación Carlos Slim",
+            year: "2020",
+            description: "Principios, conceptos y pilares de la POO"
+        }
+    ]
+
+    return (
+        <section className="section-studies">
+            <h1>Education</h1>
+        
+
+        </section>
+    )
+}
